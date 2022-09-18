9b6c6b6c44071c32b4e431b4fa5f9deea2163447
HYKim <ddh963963@naver.com>
2022-09-18 20:32:48 +0900

    Feat : UserDAo 테스트 리팩토링 V2 중복제거하기 각각의 test에서 사용되던 applicationContext 초기화와 userDao 초기화 등 중복들을 @BeforeEach를 통해서 한번에 제거하자 @BeforeEach는 각 테스트가 실행될때마다 자동으로 실행해준다 또 userDao나 자주쓰는 user 같은 객체를 픽스처라고하는 하는데 이를 구성해보았음

---
+++ src/test/java/tobystudy/group/testing/v1/UserDaoV2Test.java
@@ -0,0 +1,84 @@
+package tobystudy.group.testing.v1;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.dao.EmptyResultDataAccessException;
+import tobystudy.group.testing.User;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+
+
+class UserDaoV2Test {
+
+
+    private UserDao userDao;
+    private CountingConnectionMaker ccm;
+
+    private User user1;
+    private User user2;
+    private User user3;
+
+
+    @BeforeEach
+    public void setup() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(DaoFactory.class);
+        this.ccm = ac.getBean("countingConnectionMaker", CountingConnectionMaker.class);
+        this.userDao = ac.getBean("userDao", UserDao.class);
+        user1 = new User("user1", "name1", "password1");
+        user2 = new User("user2", "name2", "password2");
+        user3 = new User("user3", "name3", "password3");
+    }
+
+    @Test
+    public void addTest() throws Exception {
+        //given
+        userDao.deleteAll();
+        //when
+        userDao.add(user1);
+        User findUser = userDao.get(user1.getId());
+
+        //then
+        assertThat(findUser).isEqualTo(user1);
+    }
+
+    @Test
+    public void countingTest() throws Exception {
+        //given
+        userDao.deleteAll();
+        userDao.add(user1);
+        User findUser = userDao.get(user1.getId());
+        //when
+        //then
+        assertThat(ccm.getCount()).isEqualTo(3L);
+    }
+
+    @Test
+    public void countTest() throws SQLException, ClassNotFoundException {
+        //given
+        userDao.deleteAll();
+        assertThat(userDao.getCount()).isEqualTo(0);
+        //when
+        //then
+        userDao.add(user1);
+        assertThat(userDao.getCount()).isEqualTo(1);
+        userDao.add(user2);
+        assertThat(userDao.getCount()).isEqualTo(2);
+        userDao.add(user3);
+        assertThat(userDao.getCount()).isEqualTo(3);
+    }
+
+    @Test
+    public void getUserFail() throws Exception {
+        //given
+        userDao.deleteAll();
+        //when
+        //then
+        assertThatThrownBy(() -> userDao.get("test")).isInstanceOf(EmptyResultDataAccessException.class);
+    }
+
+}
