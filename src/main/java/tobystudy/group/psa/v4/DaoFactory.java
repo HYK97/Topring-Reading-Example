efc1e3178428915bffdbea8ff4a0bd4da6272be3
ddh963963 <ddh963963@naver.com>
2022-10-17 22:36:49 +0900

    Feat : Translation 적용

---
+++ src/main/java/tobystudy/group/psa/v4/DaoFactory.java
@@ -0,0 +1,38 @@
+package tobystudy.group.psa.v4;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DaoFactory {
+    @Bean
+    public UserDao userDao() {
+        System.out.println("빈리턴");
+        return new UserDaoJdbc(dataSource());
+    }
+
+    @Bean
+    public UserService userService() {
+        return new UserService(userDao());
+    }
+
+
+    @Bean
+    public DataSource dataSource() {
+        final String URL = "jdbc:h2:tcp://localhost/~/toby";
+        final String USERNAME = "sa";
+        final String PASSWORD = "1234";
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(org.h2.Driver.class);
+        dataSource.setUrl(URL);
+        dataSource.setUsername(USERNAME);
+        dataSource.setPassword(PASSWORD);
+
+        return dataSource;
+    }
+
+}
