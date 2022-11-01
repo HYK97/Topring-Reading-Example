b3689687c97dbf8f381fd128355d640d83ade6d5
ddh963963 <ddh963963@naver.com>
2022-11-02 00:09:01 +0900

    Test: PointcutTest class 필터 테스트

--- src/test/java/tobystudy/group/aop/pointcut/PointcutTest.java
+++ src/test/java/tobystudy/group/aop/pointcut/PointcutTest.java
@@ -1,11 +1,20 @@
 package tobystudy.group.aop.pointcut;
 
 
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.ClassFilter;
 import org.springframework.aop.Pointcut;
 import org.springframework.aop.framework.ProxyFactoryBean;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
+import tobystudy.group.aop.v2.proxy_factory_bean.Hello;
+import tobystudy.group.aop.v2.proxy_factory_bean.HelloTarget;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class PointcutTest {
     @Test
@@ -19,10 +28,11 @@ public class PointcutTest {
         };
 
         pointcut.setMappedName("sayH*");
-
-
         //when
 
+        checkAdvice(new HelloTarget(), pointcut, true);
+        checkAdvice(new HelloWorld(), pointcut, false);
+        checkAdvice(new HelloTT(), pointcut, true);
         //then
 
     }
@@ -30,7 +40,61 @@ public class PointcutTest {
     private void checkAdvice(Object target, Pointcut pointcut, boolean adviced) {
         ProxyFactoryBean pfBean = new ProxyFactoryBean();
         pfBean.setTarget(target);
+        pfBean.addAdvisor(new DefaultPointcutAdvisor(pointcut, new UpperHandler()));
+        Hello proxy = (Hello) pfBean.getObject();
+        if (adviced) {
+            assertThat(proxy.sayHello("hy")).isEqualTo("HELLO HY");
+            assertThat(proxy.sayHi("hy")).isEqualTo("HI HY");
+            assertThat(proxy.sayThankYou("hy")).isEqualTo("ThankYou hy");
+        } else {
+            assertThat(proxy.sayHello("hy")).isEqualTo("Hello hy");
+            assertThat(proxy.sayHi("hy")).isEqualTo("Hi hy");
+            assertThat(proxy.sayThankYou("hy")).isEqualTo("ThankYou hy");
+        }
 
+    }
+
+    static class UpperHandler implements MethodInterceptor {
+        @Override
+        public Object invoke(MethodInvocation invocation) throws Throwable {
+            String data = (String) invocation.proceed();
+            return Objects.requireNonNull(data).toUpperCase();
+        }
+    }
+
+    static class HelloWorld implements Hello {
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "ThankYou " + name;
+        }
+
+    }
+
+    static class HelloTT implements Hello {
+        @Override
+        public String sayHello(String name) {
+            return "Hello " + name;
+        }
+
+        @Override
+        public String sayHi(String name) {
+            return "Hi " + name;
+        }
+
+        @Override
+        public String sayThankYou(String name) {
+            return "ThankYou " + name;
+        }
 
     }
 }
